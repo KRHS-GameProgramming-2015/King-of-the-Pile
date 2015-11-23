f3d011f44d591a1fb9e1f75f059094ead6bb84c1
rcnerd <hunter.g.wells@gmail.com>
2015-11-23 12:23:31 -0500

    Updated base code in console. has some transfer errors

--- KingOTP.py
+++ KingOTP.py
@@ -1,71 +1,130 @@
-import sys, pygame, math
+import sys, pygame, math, random
+from Ball import *
+from Player import *
+#from Predator import *
 pygame.init()
-
-def distance(pt1, pt2):
-    x1 = pt1[0]
-    y1 = pt1[1]
-    x2 = pt2[0]
-    y2 = pt2[1]
-    return math.sqrt((x1-x2)**2+(y1-y2)**2)
 
 clock = pygame.time.Clock()
 
-width = 800 
-height = 600
+width = 600 #1280 
+height = 600 #1024
 size = width, height
 
-speed1x = 5
-speed1y = 5
-speed1 = [speed1x, speed1y]
+r = 255
+g = 255
+b = 255
 
-speed2x = 3
-speed2y = 3
-speed2 = [speed2x, speed2y]
+##bgColor = BgColor()
 
-bgColor = r,g,b = 0, 0, 0
+screen = pygame.display.set_mode(size)#pygame.FULLSCREEN )
 
-screen = pygame.display.set_mode(size)
+balls = []
+ballTimer = 0
+ballTimerMax = .685 * 60
 
-ball1 = pygame.image.load("ball1.png")
-ball1rect = ball1.get_rect()
-ball1radius = ball1rect.width/2 - 2
-ball2 = pygame.image.load("ball2.png")
-ball2rect = ball2.get_rect()
-ball2radius = ball2rect.width/2 - 2 
-ball2rect = ball2rect.move([200,200])
+player = PlayerBall(["ball.png"],[10,10],[width/2, height/2])
+
+
+
+count = 0
+while count < 4:
+
+    ballTimer = 0
+    ballSpeed = [random.randint(-5, 5),
+                 random.randint(-5, 5)]
+    if ballSpeed[0] == 0:
+        ballSpeed[0] = 1
+    if ballSpeed[1] == 0:
+        ballSpeed[1] = 1
+        
+    ballPos = [random.randint(100, width-100),
+                 random.randint(100, height-100)]
+    #ballImage = ballImages[random.randint(0, len(ballImages)-1)]
+    balls += [Ball(["ball.png",
+                    "balla.png",
+                    "ballb.png",
+                    "ballc.png"],
+                   ballSpeed,
+                   ballPos)]
+    count += 1
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: 
             sys.exit()
-            
-    ball1rect = ball1rect.move(speed1)
-    if ball1rect.left < 0 or ball1rect.right > width:
-        speed1[0] = -speed1[0]
-    if ball1rect.top < 0 or ball1rect.bottom > height:
-        speed1[1] = -speed1[1]
-        
-    ball2rect = ball2rect.move(speed2)
-    if ball2rect.left < 0 or ball2rect.right > width:
-        speed2[0] = -speed2[0]
-    if ball2rect.top < 0 or ball2rect.bottom > height:
-        speed2[1] = -speed2[1]
-        
-    if ball1rect.right > ball2rect.left and ball1rect.left < ball2rect.right:
-        if ball1rect.bottom > ball2rect.top and ball1rect.top < ball2rect.bottom:
-            if ball1radius + ball2radius > distance(ball1rect.center, ball2rect.center):
-                if ball1rect.center[0] < ball2rect.center[0] and speed1[0] > 0:
-                    speed1[0] = -speed1[0]
-                if ball2rect.center[0] < ball1rect.center[0] and speed2[0] > 0:
-                    speed1[1] = -speed1[1]
-                if ball1rect.center[1] < ball2rect.center[1] and speed1[1] > 0:
-                    speed2[0] = -speed2[0]
-                if ball2rect.center[1] < ball2rect.center[1] and speed2[1] > 0:
-                    speed2[1] = -speed2[1]
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                player.go("up")
+            elif event.key == pygame.K_DOWN:
+                player.go("down")
+            elif event.key == pygame.K_LEFT:
+                player.go("left")
+            elif event.key == pygame.K_RIGHT:
+                player.go("right")
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_UP:
+                player.go("stop up")
+            elif event.key == pygame.K_DOWN:
+                player.go("stop down")
+            elif event.key == pygame.K_LEFT:
+                player.go("stop left")
+            elif event.key == pygame.K_RIGHT:
+                player.go("stop right")
     
-    bgColor = r,g,b
-    screen.fill(bgColor)
-    screen.blit(ball1, ball1rect)
-    screen.blit(ball2, ball2rect)
+    mLocation = pygame.mouse.get_pos()
+    player.follow(mLocation)
+    
+    ballTimer += 1
+    if ballTimer >= ballTimerMax:
+        ballTimer = 0
+        ballSpeed = [random.randint(-5, 5),
+                     random.randint(-5, 5)]
+        if ballSpeed[0] == 0:
+            ballSpeed[0] = 1
+        if ballSpeed[1] == 0:
+            ballSpeed[1] = 1
+            
+        ballPos = [random.randint(100, width-100),
+                     random.randint(100, height-100)]
+        balls += [Ball(["ball.png",
+                        "balla.png",
+                        "ballb.png",
+                        "ballc.png"],
+                       ballSpeed,
+                       ballPos)]
+        #print len(balls), clock.get_fps()
+    
+    player.update(size)
+    
+    for ball in balls:
+        ball.update(size)
+    
+    for first in balls:
+        if player.collideBall(first):
+             first.die()
+        else:
+            for second in balls:
+                if first != second:
+                    first.collideBall(second)
+                    
+    for ball in balls:
+        if not ball.living:
+            balls.remove(ball)
+                    
+    
+    #bgColor.fade()
+    #screen.fill(bgColor.color())
+    
+    for ball in balls:
+        screen.blit(ball.image, ball.rect)
+    screen.blit(player.image, player.rect)
     pygame.display.flip()
+    
+    if len(balls) == 0:
+        count = 0
+        while count <100:
+            print "YOU WON!!!"
+            count += 1
+        sys.exit()
+        
     clock.tick(60)
